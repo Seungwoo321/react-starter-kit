aba47b1fb84237a2faf0280e4e55e27992315edb
Seungwoo321 <seungwoo321@gmail.com>
2023-05-26 18:09:25 +0900

    docs: update readme

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-    // env: { browser: true, es2020: true },
+    env: { browser: true, es2020: true },
     extends: [
         'standard',
         'standard-jsx',
@@ -10,8 +10,8 @@ module.exports = {
     settings: { react: { version: '18.2' } },
     plugins: ['react-refresh'],
     rules: {
-        // 'react-refresh/only-export-components': 'warn',
         indent: ['error', 4],
+        'react-refresh/only-export-components': 'warn',
         'no-tabs': 0,
         'react/jsx-indent': [2, 4, { checkAttributes: true, indentLogicalExpressions: true }],
         'react/jsx-indent-props': [2, 4],
